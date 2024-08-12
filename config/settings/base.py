9dc5070e4afda9eac04bf407d8c53d235e8771eb
rajatrjsharma <merajatsharma@outlook.com>
2024-08-13 02:59:00 +0530

    Test changes and pagination addition

--- config/settings/base.py
+++ config/settings/base.py
@@ -154,6 +154,8 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 SPECTACULAR_SETTINGS = {
