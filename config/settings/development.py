b013920b8b5ce3b40b013c8733c726d7bb156fb1
rajatrjsharma <merajatsharma@outlook.com>
2024-08-12 13:37:33 +0530

    Update allowed hosts

--- config/settings/development.py
+++ config/settings/development.py
@@ -2,7 +2,7 @@ from .base import *
 
 
 DEBUG = True
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '13.127.195.54']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
