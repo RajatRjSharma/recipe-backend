8b932a99c16b686d2f19524edf5517399146c001
rajatrjsharma <merajatsharma@outlook.com>
2024-08-12 15:55:20 +0530

    Update for machine port

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 
 DEBUG = True
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '13.127.195.54', 'ec2-13-127-195-54.ap-south-1.compute.amazonaws.com']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
